323ceadd4312b2cd9ebfb651168ad5ec4696bb0d
Max Metcalf <mmetcalf@simplenexus.com>
2021-12-01 22:18:54 -0700

    Add test data

--- 02/part_1.rb
+++ 02/part_1.rb
@@ -1,17 +1,21 @@
-f = File.open("./02/data.txt")
+files = ['./02/test_data.txt', './02/data.txt']
 
-data = f.read.split("\n")
-horizontal = 0
-vertical = 0
-(0...data.length).each do |i|
-  direction, amount = data[i].split(' ')
-  if direction == 'forward'
-    horizontal += amount.to_i
-  elsif direction == 'down'
-    vertical += amount.to_i
-  elsif direction == 'up'
-    vertical -= amount.to_i
+files.each do |file_name|
+  f = File.open(file_name)
+
+  data = f.read.split("\n")
+  horizontal = 0
+  vertical = 0
+  (0...data.length).each do |i|
+    direction, amount = data[i].split(' ')
+    if direction == 'forward'
+      horizontal += amount.to_i
+    elsif direction == 'down'
+      vertical += amount.to_i
+    elsif direction == 'up'
+      vertical -= amount.to_i
+    end
   end
-end
 
-p horizontal * vertical
+  p horizontal * vertical
+end
