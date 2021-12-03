dbe28a4bed8acfca2de3362441cdc6bde8e12b56
Max Metcalf <mmetcalf@simplenexus.com>
2021-12-02 22:48:25 -0700

    Cleaned up part 2

--- 03/part_2.rb
+++ 03/part_2.rb
@@ -1,81 +1,41 @@
 files = ['./03/test_data.txt', './03/data.txt']
-# files = ['./03/test_data.txt']
 
 files.each do |file_name|
   file = File.open(file_name)
   d = file.read.split("\n")
 
   # VARIABLES
-  gamma = []
-  eps = []
-
-  ones = []
-  zeros = []
-
-  # LOOP THROUGH DATA
-  d.each_with_index do |row, i|
-    (0...row.length).each do |num|
-      ones[num] ||= 0
-      zeros[num] ||= 0
-
-      ones[num] += 1 if row[num] == '1'
-      zeros[num] += 1 if row[num] == '0'
-    end
-  end
-
-  (0...ones.length).each do |i|
-    gamma[i] = ones[i] >= zeros[i] ? '1' : '0'
-    eps[i] = ones[i] < zeros[i] ? '1' : '0'
-  end
-
-  rows = d
   oxy = []
-  co2 = []
-  skipped_indices = []
-  (0...ones.length).each do |i|
-    x = 0
-    y = 0
-    d.each_with_index do |row, j|
-      next if skipped_indices.include?(j)
-      x += 1 if row[i] == '1'
-      y += 1 if row[i] == '0'
-    end
-    d.each_with_index do |row, j|
-      next if skipped_indices.include?(j)
-      if x >= y
-        skipped_indices << j if row[i] == '0'
-      else
-        skipped_indices << j if row[i] == '1'
-      end
-    end
-    if x >= y
+
+  # LOOP THROUGH DATA\
+  oxy_rows = d
+  (0...d[0].length).each do |i|
+    ones = oxy_rows.select{ |row| row[i] == '1'}.length
+    zeros = oxy_rows.select{ |row| row[i] == '0'}.length
+
+    if ones >= zeros
+      oxy_rows = oxy_rows.select { |row| row[i] == '1' }
       oxy << '1'
     else
+      oxy_rows = oxy_rows.select { |row| row[i] == '0' }
       oxy << '0'
     end
   end
 
-  skipped_indices = []
-  (0...ones.length).each do |i|
-    break if skipped_indices.length >= d.length - 1
-    x = 0
-    y = 0
-    d.each_with_index do |row, j|
-      next if skipped_indices.include?(j)
-      x += 1 if row[i] == '1'
-      y += 1 if row[i] == '0'
+  co2_rows = d
+  (0...d[0].length).each do |i|
+    ones = co2_rows.select{ |row| row[i] == '1'}.length
+    zeros = co2_rows.select{ |row| row[i] == '0'}.length
+
+    if ones < zeros
+      co2_rows = co2_rows.select { |row| row[i] == '1' }
+    else
+      co2_rows = co2_rows.select { |row| row[i] == '0' }
     end
-    d.each_with_index do |row, j|
-      next if skipped_indices.include?(j)
-      if x < y
-        skipped_indices << j if row[i] == '0'
-      else
-        skipped_indices << j if row[i] == '1'
-      end
-    end
+
+    break if co2_rows.length == 1
   end
-  remaining = (0...d.length).to_a - skipped_indices
 
   # PRINT RESULT
-  p d[remaining[0]].to_i(2) * oxy.join('').to_i(2)
+  p oxy.join('').to_i(2) * co2_rows[0].to_i(2)
 end
