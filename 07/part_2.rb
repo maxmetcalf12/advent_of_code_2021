4657bbc29e02a382a0c74e22ed694ff4a663e50a
Max Metcalf <mmetcalf@simplenexus.com>
2021-12-06 22:13:06 -0700

    Day 7

---
+++ 07/part_2.rb
@@ -0,0 +1,31 @@
+require 'byebug'
+
+files = ['./07/test_data.txt']
+files = ['./07/test_data.txt', './07/data.txt']
+
+def calculate_fuel(change)
+  return 0 if change == 0
+  total = 0
+
+  (1..change).each do |i|
+    total += i
+  end
+
+  total
+end
+
+files.each do |file_name|
+  file = File.open(file_name)
+  d = file.read.split("\n")
+
+  # VARIABLES
+  crabs = d[0].split(',').map{ |x| x.to_i }
+  min_total = Float::INFINITY
+  
+  (crabs.min..crabs.max).each do |x|
+    total = crabs.map { |y| calculate_fuel((y - x).abs) }
+    min_total = [min_total, total.sum].min
+  end
+
+  p min_total
+end
