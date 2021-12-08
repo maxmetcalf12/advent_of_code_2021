4f409e6177dc119793d195ddad81529eeb11e28e
Max Metcalf <mmetcalf@simplenexus.com>
2021-12-07 21:59:39 -0700

    Day 8 prep

---
+++ 08/part_2.rb
@@ -0,0 +1,20 @@
+require 'byebug'
+
+files = ['./08/test_data.txt']
+# files = ['./08/test_data.txt', './08/data.txt']
+
+files.each do |file_name|
+  file = File.open(file_name)
+  d = file.read.split("\n")
+
+  # VARIABLES
+  crabs = d[0].split(',').map{ |x| x.to_i }
+  min_total = Float::INFINITY
+  
+  (crabs.min..crabs.max).each do |x|
+    total = crabs.map { |y| (y - x).abs }
+    min_total = [min_total, total.sum].min
+  end
+
+  p min_total
+end
