82d81abec26c823c2385afc436dd2dda750f69a2
maxmetcalf12 <maxmetcalf12@gmail.com>
2024-12-01 10:31:25 -0700

    Use transpose instead of two separate array calculations

--- 2024/01/solution.rb
+++ 2024/01/solution.rb
@@ -6,6 +6,8 @@ f = File.open('./data.txt')
 data = f.read.split("\n")
 a = data.map(&:split).map(&:first).map(&:to_i).sort
 b = data.map(&:split).map(&:last).map(&:to_i).sort
+# Equivalent to below using transpose
+# a, b = data.map(&:split).transpose.map { |x| x.map(&:to_i).sort }
 
 # PART 1
 diff = a.zip(b).map { |x, y| (x - y).abs }
