143eedcaec55d9f45a898d55655dd49d29fd99d2
maxmetcalf12 <maxmetcalf12@gmail.com>
2024-12-01 08:42:55 -0700

    Day one solution

---
+++ 2024/01/solution.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'byebug'
+f = File.open('./data.txt')
+
+data = f.read.split("\n")
+a = data.map(&:split).map(&:first).map(&:to_i).sort
+b = data.map(&:split).map(&:last).map(&:to_i).sort
+
+# PART 1
+diff = a.zip(b).map { |x, y| (x - y).abs }
+puts diff.sum
+
+# PART 2
+counts = a.map { |x| x * b.filter { |y| y == x }.count }
+puts counts.sum
